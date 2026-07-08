57c472ef0dc20f78db8e6f79e37f07770839ec7f
Alejandro <UO294431@uniovi.es>
2026-07-08 19:16:18 +0200

    GitHub Actions implementation

---
+++ docs/conf.py
@@ -0,0 +1,20 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../backend'))
+
+project = 'EPI-DevorApp'
+copyright = '2026, UO294431'
+author = 'UO294431'
+release = '0.1.0'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,21 @@
+Welcome to EPI-DevorApp's documentation!
+=========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+Backend API Reference
+=====================
+
+.. automodule:: app.main
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
